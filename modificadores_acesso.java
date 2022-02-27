40d9bb71da95f14ac700106db44499d72f49b6c6
Graziela Guimarães <grazielaguimaraes1988@gmail.com>
2022-02-27 16:27:35 -0300

    modificadores de acesso

---
+++ modificadores_acesso.java
@@ -0,0 +1,40 @@
+﻿package atributosModificadores
+
+/**
+ *
+ * @author Graziela Guimarães
+ */
+
+public class ModificadoresDeAcesso {
+    
+    public String publico;
+    protected String protegido;
+    private String privado;
+
+    public String getPublico() {
+        return publico;
+    }
+
+    public void setPublico(String publico) {
+        this.publico = publico;
+    }
+
+    public String getProtegido() {
+        return protegido;
+    }
+
+    public void setProtegido(String protegido) {
+        this.protegido = protegido;
+    }
+
+    public String getPrivado() {
+        return privado;
+    }
+
+    public void setPrivado(String privado) {
+        this.privado = privado;
+    }
+    
+    
+    
+}
