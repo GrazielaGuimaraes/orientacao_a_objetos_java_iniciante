c89a622b5f6a21d871ea422a25b0ad06b357064e
Graziela Guimarães <grazielaguimaraes1988@gmail.com>
2022-02-27 15:59:30 -0300

    commit inicial

---
+++ cliente.java
@@ -0,0 +1,23 @@
+﻿package modelos;
+
+/**
+ *
+ * @author Graziela Guimarães
+ */
+
+//Herda de pessoa
+public class Cliente extends Pessoa {
+    
+    private boolean statusCliente;
+
+    public boolean isStatusCliente() {
+        return statusCliente;
+    }
+
+    public void setStatusCliente(boolean statusCliente) {
+        this.statusCliente = statusCliente;
+    }
+    
+    
+    
+}
