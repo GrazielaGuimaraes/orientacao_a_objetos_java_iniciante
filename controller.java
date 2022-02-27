9dc86aaae21ee91387ca8ce7ac890c8d425d7486
Graziela Guimarães <grazielaguimaraes1988@gmail.com>
2022-02-27 15:52:35 -0300

    commit inicial

---
+++ controller.java
@@ -0,0 +1,31 @@
+﻿package controles;
+
+import modelos.Usuario;
+
+/**
+ *
+ * @author Graziela Guimarães
+ */
+public class ClasseController {
+    
+    public static void main(String[] args) {
+        
+        Usuario usuario = new Usuario();
+        
+        //Atributos herdados da classe mãe
+        usuario.setId(1);
+        usuario.setNome("Graziela Guimarães");
+        usuario.setTelefone("0000-0000");
+        usuario.setEmail("graziela@exemplo.com");
+        usuario.setEndereco("Rua tal tal, número 000");
+        
+        //Atributos específicos da classe filha
+        usuario.setLogin("admin");
+        usuario.setSenhaAcesso("123");
+        
+        //Chama o métodos que lista no console o contéudo dos atributos de usuario
+        usuario.mostraDados();
+        
+    }
+    
+}
---
+++ usuario.java
@@ -0,0 +1,37 @@
+﻿package modelos;
+
+/**
+ *
+ * @author Graziela Guimarães
+ */
+
+//Classe filha (especificação) herda da classe Pessoa
+public class Usuario extends Pessoa{
+    
+    private String login;
+    private String senhaAcesso;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenhaAcesso() {
+        return senhaAcesso;
+    }
+
+    public void setSenhaAcesso(String senhaAcesso) {
+        this.senhaAcesso = senhaAcesso;
+    }
+    
+    public void mostraDados(){
+        System.out.println("Nome: " + this.getNome());
+        System.out.println("Email: " + this.getEmail());
+        System.out.println("Telefone: " + this.getTelefone());
+        System.out.println("Endereço: " + this.getEndereco());
+    }
+    
+}
