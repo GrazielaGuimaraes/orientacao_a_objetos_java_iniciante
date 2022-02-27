0114a17757001ed8882a2e035fa0da0cb70692e3
Graziela Guimarães <grazielaguimaraes1988@gmail.com>
2022-02-27 16:10:47 -0300

    alteração 1 na classe Controller

--- controller.java
+++ controller.java
@@ -1,5 +1,6 @@
 ﻿package controles;
 
+import modelos.Cliente;
 import modelos.Usuario;
 
 /**
@@ -25,6 +26,37 @@ public class ClasseController {
         
         //Chama o métodos que lista no console o contéudo dos atributos de usuario
         usuario.mostraDados();
+        
+        
+        
+        /****Intância de cliente*/
+        Cliente cliente = new Cliente();
+        
+        //Atributos herdados da classe mãe
+        cliente.setId(1);
+        cliente.setNome("Maria de Souza");
+        cliente.setTelefone("1111-0000");
+        cliente.setEmail("maria@exemplo.com");
+        cliente.setEndereco("Rua das Marias, 001");
+        
+        //Atributos específicos da classe filha
+        cliente.setStatusCliente(true);
+        
+        System.out.println("\n/********Mostrando os dados de cliente no console********/\n");
+        System.out.println("Nome do cliente: " + cliente.getNome());
+        System.out.println("Telefone do cliente: " + cliente.getTelefone());
+        System.out.println("Email do cliente: " + cliente.getEmail());
+        System.out.println("Endereço do cliente: " + cliente.getEndereco());
+        System.out.println("ID do cliente: " + cliente.getId());
+        
+        System.out.println("Status do cliente: " + cliente.isStatusCliente());
+        
+        //Mostrando mensagem através do resultado do status do cliente
+        if(cliente.isStatusCliente() == true){
+            System.out.println("Status do cliente atualizado: Ativo" );
+        }else{
+            System.out.println("Status do cliente atualizado: Inativo" );
+        }
         
     }
     
